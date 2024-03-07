f65904cbb3cbfb45ba92afdbc8fcc4cc0bee2dd6
louis <louisdx24@icloud.com>
2024-03-07 21:44:17 +0800

    1.完成jinja基礎語法 2.輸出書本資訊

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
-books = {1: "Python book", 2.0: "Java book", 3: "Flask book"}
+books = {1: "Python book", 2: "Java book", 3: "Flask book"}
 app = Flask(__name__)
+
+# 一個函式綁一個 router
 
 
 @app.route("/sum/x=<a>&y=<b>")
@@ -25,7 +27,30 @@ def get_book(id):
 
 @app.route("/books")
 def get_books():
-    return books
+    books = {
+        1: {
+            "name": "Python book",
+            "price": 299,
+            "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
+        },
+        2: {
+            "name": "Java book",
+            "price": 399,
+            "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
+        },
+        3: {
+            "name": "C# book",
+            "price": 499,
+            "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
+        },
+    }
+
+    for id in books:
+        print(
+            f'{id}: 名稱: {books[id]["name"]} 價格: {books[id]["price"]} 圖片: {books[id]["image_url"]}'
+        )
+
+    return render_template("books.html", books=books)
 
 
 # 一個函式可綁多個 router
@@ -34,7 +59,8 @@ def get_books():
 def index():
     print(datetime.now())
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f"<h1>Hello Flask!{now}</h1>"
+
+    return render_template("index.html", time=now, name="Jerry")
 
 
 # ctr+c 斷開 server
